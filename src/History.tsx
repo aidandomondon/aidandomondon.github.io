6de623c74630f5d82c16dc19f1b9ee31c5a99d10
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2024-12-15 13:58:34 -0500

    Makes timeline not alternate

--- src/History.tsx
+++ src/History.tsx
@@ -6,61 +6,65 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineDot from '@mui/lab/TimelineDot';
-import { CUSTOM_DARK_BLUE, CUSTOM_WHITE } from './colors'
+import { CUSTOM_DARK_BLUE } from './colors'
 
+const textStyle: React.CSSProperties = {
+    color: CUSTOM_DARK_BLUE,
+    fontFamily: 'Times New Roman'
+};
 export default function History() {
     return (
         <div>
-            <Timeline position="alternate">
+            <Timeline sx={{justifyContent: 'left'}}>
                 <TimelineItem>
-                    <TimelineOppositeContent color={CUSTOM_DARK_BLUE} fontFamily={'Times New Roman'}>
+                    <TimelineOppositeContent style={textStyle}>
                         Winter 2025
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot />
                         <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Last Semester @ Northeastern University</TimelineContent>
+                    <TimelineContent style={textStyle}>Last Semester @ Northeastern University</TimelineContent>
                 </TimelineItem>
                 <TimelineItem>
-                    <TimelineOppositeContent color={CUSTOM_DARK_BLUE} fontFamily={'Times New Roman'}>
+                    <TimelineOppositeContent style={textStyle}>
                         Fall 2024
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot />
                         <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Data Analysis Consultant @ UCI MUST Lab</TimelineContent>
+                    <TimelineContent style={textStyle}>Data Analysis Consultant @ UCI MUST Lab</TimelineContent>
                 </TimelineItem>
                 <TimelineItem>
-                    <TimelineOppositeContent color={CUSTOM_DARK_BLUE}>
+                    <TimelineOppositeContent style={textStyle}>
                         Winter 2024
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot />
                         <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Data Science Co-op @ Northeastern University</TimelineContent>
+                    <TimelineContent style={textStyle}>Data Science Co-op @ Northeastern University</TimelineContent>
                 </TimelineItem>
                 <TimelineItem>
-                    <TimelineOppositeContent color={CUSTOM_DARK_BLUE}>
+                    <TimelineOppositeContent style={textStyle}>
                         Spring 2023
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot />
                         <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>Software Engineer Co-op @ New Age Micro, LLC.</TimelineContent>
+                    <TimelineContent style={textStyle}>Software Engineer Co-op @ New Age Micro, LLC.</TimelineContent>
                 </TimelineItem>
                 <TimelineItem>
-                    <TimelineOppositeContent color={CUSTOM_DARK_BLUE}>
+                    <TimelineOppositeContent style={textStyle}>
                         Fall 2021
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                         <TimelineDot />
                         <TimelineConnector />
                     </TimelineSeparator>
-                    <TimelineContent>First Semester @ Northeastern University</TimelineContent>
+                    <TimelineContent style={textStyle}>First Semester @ Northeastern University</TimelineContent>
                 </TimelineItem>
             </Timeline>
         </div>
