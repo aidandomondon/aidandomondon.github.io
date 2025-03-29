e174b0480c10c32acd3cc6a50c43dfaa1cc653d4
Ananya Tadigadapa <anatadig@gmail.com>
2025-03-28 21:59:09 -0400

    revamping components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import TopBar from './TopBar';
-import Preface from './Preface';
-import Stack from '@mui/material/Stack';
-import TabView from './TabView';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Card } from '@mui/material';
-
-const background_image = require('./ai_ml.webp');
+import Header from './components/header';
+import TabsContainer from './components/tabs';
+import Container from '@mui/material/Container';
+import About from './components/about';
 
 function App() {
 
@@ -20,13 +17,11 @@ function App() {
   return (
     <ThemeProvider theme={globalMuiTheme}>
       <div className="App">
-        <Stack spacing={10}>
-          <TopBar />
-          <Preface />
-          <div style={{boxShadow: "0px -1px 10px rgba(0, 0, 0, 0.5)"}}>
-            <TabView />
-          </div>
-        </Stack>
+        <Container style={{marginTop: '5%'}}>
+          <About></About>
+          <TabsContainer></TabsContainer>
+        </Container>
+        <Header></Header>
       </div>
     </ThemeProvider>
   );
---
+++ src/components/experienceItem.tsx
@@ -0,0 +1,47 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import * as React from 'react';
+import { ReactElement } from 'react';
+
+type ExperienceItemProps = {
+  title: string
+  time: string
+  content: ReactElement
+}
+
+const Item = styled(Box)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+const HeadingItem = styled(Box)(({ theme }) => ({
+  backgroundColor: (theme).palette.primary.main,
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: 'white',
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#fff',
+  }),
+}));
+
+export default function ExperienceItem(props: ExperienceItemProps) {
+  return <Box sx={{ width: '100%' }}>
+    <Stack>
+      <Item><Stack spacing={2} direction='row'>
+        <HeadingItem>{props.time}</HeadingItem>
+        <HeadingItem>{props.title}</HeadingItem>
+      </Stack></Item>
+      <Item>
+        {props.content}
+      </Item>
+    </Stack>
+</Box>
+}
