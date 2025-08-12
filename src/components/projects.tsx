8bc816f4ff6cac0413ee21e2560b1867077cd4c6
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-08-12 19:51:34 -0400

    Makes projects cards

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,30 +1,78 @@
-import { List, ListItemText } from '@mui/material';
+import Card from '@mui/material/Card';
+import { CardActions, CardContent } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import ConstructionIcon from '@mui/icons-material/Construction';
+import { IconButton } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LaunchIcon from '@mui/icons-material/Launch';
+import { ReactNode } from 'react';
 
-const listStyle = { listStyleType: 'disc', pl: 4 }
-const listItemStyle = { display: 'list-item' }
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  cardActions: ReactNode;
+  sx?: object;
+}
+
+function ProjectCard(props: ProjectCardProps) {
+  return (
+    <Card
+      variant='outlined'
+      sx={{
+        borderRadius: '15px',
+        borderStyle: 'solid',
+        backgroundColor: 'rgba(0,0,0,0.25)',
+        ...props.sx
+      }}
+    >
+      <CardContent>
+        <Typography variant='body2' align='left'>
+          <b>{props.title}</b>
+        </Typography>
+        <Typography variant='body2' align='left'>
+          {props.description}
+        </Typography>
+      </CardContent>
+      {props.cardActions}
+    </Card>
+  )
+}
 
 export default function Projects() {
   return (
-    <Stack direction='column' alignItems='start'>
+    <Stack direction='column' alignItems='start' spacing={1}>
       <Stack direction='row' spacing={1} alignItems='center'>
         <ConstructionIcon fontSize='large' />
         <Typography variant='body1' align='left'>Recent Projects</Typography>
       </Stack>
-      <List sx={listStyle}>
-        <ListItemText sx={listItemStyle}>
-          <Typography variant='body2' align='left'>
-            <b>Come Back Sunday:</b> mental-health-aware social media app
-          </Typography>
-        </ListItemText>
-        <ListItemText sx={listItemStyle}>
-          <Typography variant='body2' align='left'>
-            <b>Tarragon:</b> privacy-focused, offline chatbot that can analyze users' documents
-          </Typography>
-        </ListItemText>
-      </List>
+      <Stack direction='row' spacing={1}>
+        <ProjectCard 
+          title="Come Back Sunday"
+          description="Mental-health-conscious social media app"
+          cardActions={(
+            <CardActions>
+              <IconButton color="inherit" href='https:comebacksunday.pythonanywhere.com'>
+                <LaunchIcon/>
+              </IconButton>
+              <IconButton color="inherit" href='https://github.com/aidandomondon/comebacksunday'>
+                <GitHubIcon/>
+              </IconButton>
+            </CardActions>
+          )}
+        />
+        <ProjectCard 
+          title="Tarragon" 
+          description="Privacy-focused, offline chatbot that reasons over users' documents"
+          cardActions={(
+            <CardActions>
+              <IconButton color="inherit" href='https://github.com/aidandomondon/tarragon'>
+                <GitHubIcon/>
+              </IconButton>
+            </CardActions>
+          )}
+        />
+      </Stack>
     </Stack>
   )
 }
